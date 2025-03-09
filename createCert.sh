9bfd1490b2932fdfd91a8f7b0ab2b53b766497f3
tulkuajam <tulkuajam@hotmail.com>
2025-03-09 11:02:13 +0800

    update createCert

--- createCert.sh
+++ createCert.sh
@@ -1,1 +1,5 @@
-openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365
+# openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365
+
+openssl genpkey -algorithm RSA -out key.pem -pkeyopt rsa_keygen_bits:2048
+
+openssl req -x509 -new -key key.pem -out cert.pem -days 365 -subj "/CN=192.168.177.132"
