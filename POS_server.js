87824bf40b5e1025c15067620914588ff1077cb1
tulkuajam <tulkuajam@hotmail.com>
2025-03-10 13:42:08 +0800

    a

--- POS_server.js
+++ POS_server.js
@@ -79,7 +79,9 @@ const port = 3000; // Standard HTTPS port
 // const ip   = '192.168.1.70'
 // const ip   = '10.241.129.201' // kalsang iphon
 const ip   = '10.241.103.162' //thubten nyima
-server.listen(port, ip, () => {
+server.listen(port, 
+  // ip, 
+  () => {
   console.log(`HTTP server listening on ${ip}:${port}`);
 });
 /*
