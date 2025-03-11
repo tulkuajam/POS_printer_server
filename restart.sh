cb4a404e3ec27d42eaecca37cca3aaab2d162a65
tulkuajam <tulkuajam@hotmail.com>
2025-03-11 21:17:54 +0800

    a

--- restart.sh
+++ restart.sh
@@ -1,4 +1,6 @@
 rm -rf POS_printer_server
 git clone https://github.com/tulkuajam/POS_printer_server
 # node ./POS_printer_server/POS_server.js
-node ./POS_printer_server/POS_server_express.js
+# node ./POS_printer_server/POS_server_express.js
+node ./POS_printer_server/POS_server_http.js
+
