9beafa82a85e7a8861ab22fc341937786b646610
tulkuajam <tulkuajam@hotmail.com>
2025-03-12 17:34:27 +0800

    a

--- POS_server_http.js
+++ POS_server_http.js
@@ -180,21 +180,21 @@ function printImageToNetworkPrinter(printerIP, port, imagePath, density = 'd24')
           .style('bu')
           .size(1, 1)
           .text('The quick brown fox jumps over the lazy dog')
-          .text('敏捷的棕色狐狸跳过懒狗')
-          .barcode('1234567', 'EAN8')
-          .table(["One", "Two", "Three"])
-          .tableCustom(
-            [
-              { text:"Left", align:"LEFT", width:0.33, style: 'B' },
-              { text:"Center", align:"CENTER", width:0.33},
-              { text:"Right", align:"RIGHT", width:0.33 }
-            ],
-            { encoding: 'cp857', size: [1, 1] } // Optional
-          )
-          .qrimage('https://github.com/song940/node-escpos', function(err){
-            this.cut();
-            this.close();
-          });
+          // .text('敏捷的棕色狐狸跳过懒狗')
+          // .barcode('1234567', 'EAN8')
+          // .table(["One", "Two", "Three"])
+          // .tableCustom(
+          //   [
+          //     { text:"Left", align:"LEFT", width:0.33, style: 'B' },
+          //     { text:"Center", align:"CENTER", width:0.33},
+          //     { text:"Right", align:"RIGHT", width:0.33 }
+          //   ],
+          //   { encoding: 'cp857', size: [1, 1] } // Optional
+          // )
+          // .qrimage('https://github.com/song940/node-escpos', function(err){
+          //   this.cut();
+          //   this.close();
+          // });
         // });
 
         
