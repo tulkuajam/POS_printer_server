37645532278ecb5c7d2f73487d195d28b39d0fe1
tulkuajam <tulkuajam@hotmail.com>
2025-03-12 16:53:14 +0800

    l

--- POS_server_http.js
+++ POS_server_http.js
@@ -1,6 +1,6 @@
 const http = require('http');
-const ThermalPrinter = require('node-thermal-printer').printer;
-const PrinterTypes = require('node-thermal-printer').types;
+// const ThermalPrinter = require('node-thermal-printer').printer;
+// const PrinterTypes = require('node-thermal-printer').types;
 
 // const Jimp = require("jimp");
 
