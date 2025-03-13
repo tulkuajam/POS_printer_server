318249b2a1c6064bca5782af4c28fdd4dcb78ddf
tulkuajam <tulkuajam@hotmail.com>
2025-03-13 09:32:58 +0800

    a

---
+++ POS_server_RPE.js
@@ -0,0 +1,36 @@
+const escpos = require('escpos');
+const { ReceiptPrinterEncoder } = require('escpos');
+
+const PRINTER_IP = "192.168.177.128"; // Change this to your printer's IP
+const PRINTER_PORT = 9100; // Most ESC/POS network printers use port 9100
+
+// Create a network device
+const device = new escpos.Network(PRINTER_IP, PRINTER_PORT);
+
+// Initialize printer
+const printer = new escpos.Printer(device);
+
+// Create encoder
+const encoder = new ReceiptPrinterEncoder();
+encoder.initialize();
+encoder.text("Hello, World!");
+encoder.newline();
+encoder.bold(true);
+encoder.text("Bold Text Example");
+encoder.bold(false);
+encoder.newline();
+encoder.align('center');
+encoder.text("Center Aligned Text");
+encoder.newline();
+encoder.cut();
+
+const encodedData = encoder.encode();
+
+console.log(encodedData)
+
+// Send data to the printer
+device.open(() => {
+  printer.raw(encodedData);
+  printer.cut();
+  printer.close();
+});
--- restart.sh
+++ restart.sh
@@ -2,5 +2,5 @@ rm -rf POS_printer_server
 git clone https://github.com/tulkuajam/POS_printer_server
 # node ./POS_printer_server/POS_server.js
 # node ./POS_printer_server/POS_server_express.js
-node ./POS_printer_server/POS_server_http.js
+node ./POS_printer_server/POS_server_RPE.js
 
